9c7c8363b3fc270f9bcb378e2d665d0c0a3b8150
bemake-pycharm <271374667@qq.com>
2024-05-19 20:22:04 +0800

    Docs: 为README增加了更多的说明 Docs: 为关于页面增加了反诈骗说明

--- src/about.py
+++ src/about.py
@@ -8,24 +8,26 @@ about_txt: str = """
 </head>
 
 <body>
-    <h1 style="text-align: center;">VideoMosaic</h1>
-    <p style="text-align: center;">一个简陋但不简单的视频拼接工具</p>
-    <p style="text-align: center; ">支持多种视频格式，
-        支持多个视频拼接，
-        <strong>自研去黑边算法，能自动去除边角Logo，字幕，动态图标等，
-            快速提取视频主体</strong>
-        支持自动将视频统一旋转到竖屏模式，自动调整视频大小到最佳，支持调整视频帧率
-    </p>
-    <p style="text-align: center;">支持视频格式：mp4, avi, flv, mov, wmv, mkv, rmvb, rm, 3gp, mpeg, asf, ts
-    </p>
-    <h3 style="text-align: center;">如何联系我?</h3>
-    <p style="text-align: center;">本人的GitHub:https://github.com/271374667</p>
-    <p style="text-align: center;">本人的邮箱:271374667@qq.com</p>
-    <p style="text-align: center;">欢迎来QQ群里面讨论:557434492</p>
+<h1 style="text-align: center;">VideoMosaic</h1>
+<p style="text-align: center;">一个简陋但不简单的视频拼接工具</p>
+<p style="text-align: center; ">支持多种视频格式，
+    支持多个视频拼接，
+    <strong>自研去黑边算法，能自动去除边角Logo，字幕，动态图标等，
+        快速提取视频主体</strong>
+    支持自动将视频统一旋转到竖屏模式，自动调整视频大小到最佳，支持调整视频帧率
+</p>
+<p style="text-align: center;">支持视频格式：mp4, avi, flv, mov, wmv, mkv, rmvb, rm, 3gp, mpeg, asf, ts
+</p>
+<h3 style="text-align: center;">如何联系我?</h3>
+<p style="text-align: center;">本人的GitHub:https://github.com/271374667</p>
+<p style="text-align: center;">本人的邮箱:271374667@qq.com</p>
+<p style="text-align: center;">欢迎来QQ群里面讨论:557434492</p>
 
-    <h3 style="text-align: center;">如何学习PySide6?</h3>
-    <p style="text-align: center;">欢迎前往B站观看本人录制的免费教程:https://space.bilibili.com/282527875</p>
-
+<h3 style="text-align: center;">如何学习PySide6?</h3>
+<p style="text-align: center;">欢迎前往B站观看本人录制的免费教程:https://space.bilibili.com/282527875</p>
+<h3 style="text-align: center">软件开源</h3>
+<p style="text-align: center">
+    本软件是开源软件，如果您是通过付费获取到的本产品请立即退款挽回您的经济损失，同时您可以随时在GitHub上找到免费的最新版本:https://github.com/271374667/VideoMosaic</p>
 
 </body>
 
