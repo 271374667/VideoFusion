8cbc3f4f3038064d26730910b305c5586aaf127a
bemake-pycharm <271374667@qq.com>
2024-10-28 11:29:47 +0800

    ✨ feat(VideoFusion.py)：添加视频融合主程序文件 📝 docs(VideoFusion.py)：记录日志配置和主应用程序逻辑

--- VideoFusion.py
+++ VideoFusion.py
@@ -6,15 +6,12 @@ from src.presenter.main_presenter import MainPresenter
 
 loguru.logger.add(LOG_FILE, rotation="1 week", retention="1 days", level="DEBUG")
 
-
+@loguru.logger.catch(reraise=True)
 def main():
-    try:
-        app = QApplication([])
-        main_presenter = MainPresenter()
-        main_presenter.get_view().show()
-        app.exec()
-    except Exception as e:
-        loguru.logger.critical(f"程序发生了未知错误: {e}")
+    app = QApplication([])
+    main_presenter = MainPresenter()
+    main_presenter.get_view().show()
+    app.exec()
 
 
 if __name__ == '__main__':
